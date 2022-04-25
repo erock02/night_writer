092d0ac03f802770d33fc078cb222b0aea4803cb
Eric Espindola <eric_e_2012@hotmail.com>
2022-04-25 01:21:38 -0700

    feat: add ability to create and write to file

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -1,3 +1,6 @@
-file = File.read("./lib/#{ARGV[0]}").split(' ')
-counter = file.length
+message_file = File.read("./lib/#{ARGV[0]}").split(' ').join()
+counter = message_file.length
 p "Created #{ARGV[1]} containing #{counter} characters"
+
+translation_file = File.open(ARGV[1], 'w')
+translation_file.write(message_file)
