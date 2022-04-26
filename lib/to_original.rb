167d59fdcafb2c09574fe8ff4f9b62bc8550e121
Eric Espindola <eric_e_2012@hotmail.com>
2022-04-26 06:01:51 -0700

    add format message method. Use format method in translate method

--- lib/to_original.rb
+++ lib/to_original.rb
@@ -13,9 +13,22 @@ class ToOriginal < ToBraille
     @original_hash[braille_array]
   end
 
-  def translate_message(message)
+  def format_message(message)
+    counter = message.length
+    formatted_array = Array.new(0)
+    i = 0
+    while i < (counter/3 - 1)
+      formatted_array[i/2] = [message[i] + message [i+1]]
+      formatted_array[i/2].concat([message[i + counter/3] + message[i + counter/3 +1]])
+      formatted_array[i/2].concat([message[i + ((2* counter)/3)] + message [i + ((2* counter)/3) +1]])
+      i += 2
+    end
+    formatted_array
+  end
+
+  def translate_message
     translation_string = ""
-    message.each do |braille|
+    format_message(@omessage).each do |braille|
       translation_string.concat(translate_braille(braille))
     end
     translation_string
