5e5314e8fd4f5b1cb3a5a20cc07fbbe7723cd771
Eric Espindola <eric_e_2012@hotmail.com>
2022-04-26 00:25:57 -0700

    updated night_reader template to get a more accurate character count

--- lib/night_reader.rb
+++ lib/night_reader.rb
@@ -1,5 +1,5 @@
-message_file = File.read("./lib/#{ARGV[0]}")
-counter = message_file.length
+message_file = File.read("./lib/#{ARGV[0]}").split(//).chomp()
+counter = message_file.length / 6
 
 translation_file = File.open("./lib/#{ARGV[1]}", 'w')
 translation_file.write(message_file)
