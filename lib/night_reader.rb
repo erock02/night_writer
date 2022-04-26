2d78e2992b14e9b3f9f26bdb07c74b59f4e81795
Eric Espindola <eric_e_2012@hotmail.com>
2022-04-26 06:03:23 -0700

    feat: add to_original class to night_reader. remove new line characters

--- lib/night_reader.rb
+++ lib/night_reader.rb
@@ -1,7 +1,11 @@
-message_file = File.read("./lib/#{ARGV[0]}").split(//).chomp()
+require './lib/to_original'
+
+message_file = File.read("./lib/#{ARGV[0]}").split(//).join().gsub("\n",'')
 counter = message_file.length / 6
 
+original  = ToOriginal.new(message_file)
+
 translation_file = File.open("./lib/#{ARGV[1]}", 'w')
-translation_file.write(message_file)
+translation_file.write(original.translate_message)
 
 p "Created #{ARGV[1]} containing #{counter} characters"
